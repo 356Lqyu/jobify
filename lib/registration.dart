6e53d4310ceed7c2e5931b7ac51a59b5e5af1406
356Lqyu <raclim356@gmail.com>
2026-03-08 01:18:22 +0800

    registration page

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart' as intl;
+///import 'package:intl/intl.dart' as intl;
 
 class Registration extends StatefulWidget {
   const Registration({super.key});
